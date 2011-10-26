91faf16ba4a74a6a3587611a54298aa64d840a0f
Ovidiu Chereches <hello@ovidiu.ch>
2011-10-26 14:10:27 +0300

    positioning under construction

--- jquery.jlaid.js
+++ jquery.jlaid.js
@@ -83,24 +83,119 @@ layout.max = function(matrix)
 
 ;(function($, window, document, undefined)
 {
+	/* Layout Grid constructor */
+
 	var Laid = function(wrapper, options)
 	{
-		this.wrapper = wrapper;
-		this.options = options;
-
 		if(!(this.children = $(wrapper).find('> li')).length)
 		{
 			return;
 		}
+		for(var i in Laid.defaults)
+		{
+			if(options[i] === undefined)
+			{
+				options[i] = Laid.defaults[i];
+			}
+		}
+		this.wrapper = wrapper;
+		this.options = options;
+
+		var that = this;
+
+		$(window).resize(function()
+		{
+			that.resize();
+		});
+		this.init();
 	};
+
+	/* static */
+
 	Laid.defaults =
 	{
 		stretch: true,
 		responsive: false
 	};
-	//Laid.prototype.
+	Laid.INFINITE = 999999;
 
-	$.fn.jLaid = function(options)
+	Laid.matrix = function(cols)
+	{
+		var matrix = [];
+
+		for(var i = 0; i < cols; i++)
+		{
+			matrix[i] = [0, 0]; // ??
+		}
+		return matrix;
+	};
+	Laid.min = function(matrix, width, total) // not good
+	{
+		var j = 0, min = this.INFINITE;
+
+		for(var i = 0; i < matrix.length; i++)
+		{
+			if(width > (total - matrix[i][0]))
+			{
+				break;
+			}
+			if(matrix[i + 1] && (matrix[i + 1][0] - matrix[i][0] < width))
+			{
+				continue;
+			}
+			if(matrix[i][1] < min)
+			{
+				j = i;
+				min = matrix[i][1];
+			}
+		}
+		console.log(j);
+		return j;
+		//return [j, matrix[j][1]];
+	};
+
+	/* prototype */
+
+	Laid.prototype.init = function()
+	{
+		$(this.wrapper).css('position', 'relative');
+
+		this.children.each(function()
+		{
+			$.data(this, 'width', $(this).outerWidth());
+			$.data(this, 'inner', $(this).outerWidth() - $(this).width());
+		});
+		this.children.css('position', 'absolute');
+
+		this.resize();
+	};
+	Laid.prototype.resize = function()
+	{
+		var matrix = Laid.matrix(this.children.length + 1);
+
+		var total = $(this.wrapper).width();
+
+		this.children.each(function()
+		{
+			var width = $.data(this, 'width');
+			var inner = $.data(this, 'inner');
+
+			var i = Laid.min(matrix, width, total);
+
+			//$(this).css('width', width - inner); // only if stretch
+
+			$(this).css('left', matrix[i][0]);
+			$(this).css('top', matrix[i][1]);
+
+			matrix[i + 1][0] += width;
+			matrix[i][1] += $(this).outerHeight();
+		});
+		console.log(matrix);
+	};
+
+	/* plugin */
+
+	$.fn.laid = function(options)
 	{
 		return this.each(function()
 		{
